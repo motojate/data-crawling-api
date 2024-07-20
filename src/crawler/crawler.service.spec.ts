d34318b31c1635f27dd93e02312866e15ed6cb9f
motojate <motojate@naver.com>
2024-07-20 22:45:57 +0900

    [FEAT] 데이터를 가지고 오는 간단한 api 작성

--- src/crawler/crawler.service.spec.ts
+++ src/crawler/crawler.service.spec.ts
@@ -12,7 +12,9 @@ describe('CrawlerService', () => {
     service = module.get<CrawlerService>(CrawlerService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('네이버의 데이터를 가지고 옵니다.', async () => {
+    const url = 'https://www.naver.com';
+    const data = await service.fetchData(url);
+    console.log(data);
   });
 });
--- src/crawler/crawler.service.ts
+++ src/crawler/crawler.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import * as puppeteer from 'puppeteer';
 
 @Injectable()
-export class CrawlerService {}
+export class CrawlerService {
+  async fetchData(url: string) {
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      await page.goto(url);
+      const headlines = await page.evaluate(() => {
+        const h1Elements = Array.from(document.querySelectorAll('h1'));
+        return h1Elements.map((element) => element.textContent);
+      });
+
+      await browser.close();
+      return headlines;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
