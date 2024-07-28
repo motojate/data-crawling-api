fef77feb66cd24a2942ea083b008d97193dfd63e
motojate <motojate@naver.com>
2024-07-28 23:01:45 +0900

    [FEAT] 문제 관련 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CrawlerModule } from './crawler/crawler.module';
 import { CacheModule } from '@nestjs/cache-manager';
+import { QuestionModule } from './question/question.module';
 
 @Module({
-  imports: [CacheModule.register(), CrawlerModule],
+  imports: [CacheModule.register(), CrawlerModule, QuestionModule],
   controllers: [AppController],
   providers: [AppService],
 })
